795f150de754d60e2f3320cd66d36c0923004d7d
Óscar Belmonte Fernández <oscar.belmonte@uji.es>
2019-04-01 18:11:37 +0200

    Utilizando lambdas para los hilos. Eliminados comentarios.

--- src/main/java/main/TestSingletonConcurrenteMejorado.java
+++ src/main/java/main/TestSingletonConcurrenteMejorado.java
@@ -2,22 +2,13 @@ package main;
 
 import singleton.SingletonConcurrenteMejorado;
 
-/**
- * Created by oscar on 13/4/16.
- */
 public class TestSingletonConcurrenteMejorado {
     private void ejecuta() {
         for(int i = 0; i < 1000; i++)
-            new Thread(new Hilo()).start();
+            new Thread(() -> SingletonConcurrenteMejorado.getInstance()).start();
         SingletonConcurrenteMejorado.instanciasConcurrentes();
     }
     public static void main(String[] args) {
         new TestSingletonConcurrenteMejorado().ejecuta();
-    }
-    private class Hilo implements Runnable {
-        @Override
-        public void run() {
-            SingletonConcurrenteMejorado.getInstance();
-        }
     }
 }
